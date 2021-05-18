93f23a35c5b3e3ddac27c113f046bf1aaef58428
EvgeniyKarachkov <cazinak341@gmail.com>
2021-05-18 23:51:07 +0300

    Third part of project

--- 100 Days fifth project/ViewController.swift
+++ 100 Days fifth project/ViewController.swift
@@ -63,7 +63,31 @@ class ViewController: UITableViewController {
     }
     
     func submit(_ answer: String) {
+            let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            if isPossible(word: lowerAnswer) {
+                if isReal(world: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = indexPath(row: 0, section: 0)
+                    tableView.indexPathForRow(at: [indexPath], with: .automatic)
+                }
+            }
+        }
     
+    }
+    
+    func isPossible(word: String) -> Bool {
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return true
+    }
+    
+    func isReal(world: String) -> Bool {
+        return true
     }
     
 }
